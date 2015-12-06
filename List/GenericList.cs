3c610bbb44c63fedf8877423a9c1bb4f4f7cbd24
dkrivosic <domagoj_70@hotmail.com>
2015-12-06 12:13:49 +0100

    generic list

---
+++ List/GenericList.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class GenericList<X> : IGenericList<X>
+    {
+        private X[] _internalStorage;
+        private int _count;
+
+        public GenericList()
+        {
+            _internalStorage = new X[4];
+            _count = 0;
+        }
+
+        public GenericList(int initialSize)
+        {
+            _internalStorage = new X[initialSize];
+            _count = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public void Add(X item)
+        {
+            if (_count >= _internalStorage.Length)
+            {
+                X[] tmpStorage = _internalStorage;
+                int newLength = _internalStorage.Length * 2;
+                _internalStorage = new X[newLength];
+                Array.Copy(tmpStorage, _internalStorage, tmpStorage.Length);
+            }
+            _internalStorage[Count] = item;
+            _count++;
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+        }
+
+        public bool Contains(X item)
+        {
+            foreach (X elem in _internalStorage)
+            {
+                if (elem.Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public X GetElement(int index)
+        {
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return _internalStorage[index];
+        }
+
+        public int IndexOf(X item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_internalStorage[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool Remove(X item)
+        {
+            int index = IndexOf(item);
+            return RemoveAt(index);
+        }
+
+        public bool RemoveAt(int index)
+        {
+            if (index >= Count || index < 0)
+            {
+                return false;
+            }
+
+            for (int i = index; i < Count - 1; i++)
+            {
+                _internalStorage[i] = _internalStorage[i + 1];
+            }
+            _count--;
+            return true;
+        }
+
+        public IEnumerator<X> GetEnumerator()
+        {
+            return new GenericListEnumerator<X>(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ List/GenericListEnumerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace List
+{
+    public class GenericListEnumerator<X> : IEnumerator<X>
+    {
+        private GenericList<X> _genericList;
+        private int _index;
+
+        public GenericListEnumerator(GenericList<X> genericList)
+        {
+            this._genericList = genericList;
+            this._index = -1;
+        }
+
+        public X Current
+        {
+            get
+            {
+                return _genericList.GetElement(_index);
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            _index++;
+            if(_index < _genericList.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+        }
+    }
+}
